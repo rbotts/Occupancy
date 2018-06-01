a259a567e86743eeeea6c9a340b9bc8fc30f6ecf
tjwieg <tjwieg@users.noreply.github.com>
2018-06-01 15:32:56 -0700

    Minor upgrade xy.R +cbind's Longitude and Latitude columns to occ.dat +definitely correct proj4 string

--- Functions/xy.R
+++ Functions/xy.R
@@ -5,5 +5,9 @@ require("proj4") #Requires installing libproj-dev on your system first
 xy <- function(x, y) {
   proj4string <- "+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m +no_defs "
   pj <- project(data.frame(x,y), proj4string, inverse=TRUE)
-  return(data.frame(lat=pj$y, long=pj$x))
+  return(data.frame(Latitude=pj$y, Longitude=pj$x))
 }
+
+#Adding Longitude and Latitude columns to occ.dat
+xy.dat <- xy(x = occ.dat$X, y = occ.dat$Y)
+occ.dat <- cbind(occ.dat, xy.dat)
