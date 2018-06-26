7182dc16fea2f4e18444ded670138236bb149459
tjwieg <tjwieg@users.noreply.github.com>
2018-06-26 16:40:29 -0700

    Started working with GIS data +plot elevation across nation with SRTM data

--- experiment.R
+++ experiment.R
@@ -6,30 +6,15 @@ require("lubridate")
 require("unmarked")
 occ.dat <- read.csv("~/Documents/MooringData-June2018-Clean.csv")
 
-#Site and month/year ----
-occ.dat["Site"] <- gsub("\\s*20\\d\\d\\s*|\\s*Spring\\s*|\\s*Summer\\s*|\\s*Fall\\s*|\\s*El\\s*|\\s*La\\s*|\\s*National Park\\s*|\\s*\\-\\s*",
-                           "",
-                           occ.dat$Survey.Name)
-# dateFormat = "%m/%d/%Y"
-# occ.dat$Date <- base::as.Date(occ.dat$Date, format = dateFormat)
-occ.dat["MonthYear"] <- sapply(X = occ.dat$Date,
+occ.dat["YearMonth"] <- sapply(X = occ.dat$Date,
                                FUN = function(x) {paste(year(x), month(x))})
 
-#Site/MonthYear Match ----
-siteList <- sort(unique(occ.dat$Site))
-myList <- sort(unique(occ.dat$MonthYear))
-matchMat <- matrix(data = NA, nrow = length(siteList), ncol = length(myList))
-for (i in 1:length(siteList)) {
-  site <- siteList[i]
-  for (j in 1:length(myList)) {
-    my <- myList[j]
+occ.dat["Region"] <- apply(X = occ.dat,
+                           MARGIN = 1,
+                           FUN = function(x) {
+                              lon <- as.numeric(x["Longitude"])
+                              lat <- as.numeric(x["Latitude"])
+                              return(paste0(round(lon, digits = 2), ";", round(lat, digits = 2)))
+                            })
 
-    if (length(subset(occ.dat$Time,
-                      occ.dat$Site == site & occ.dat$MonthYear == my))) {
-      matchMat[i,j] <- 1
-    } else matchMat[i,j] <- 0
-  }
-}
-rownames(matchMat) <- siteList
-colnames(matchMat) <- myList
-print(matchMat)
+print(unique(occ.dat$Region))
