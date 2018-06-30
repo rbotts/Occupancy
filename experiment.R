6bf29c96a195be97545ffe2a471dd3bf6b22fb18
tjwieg <tjwieg@users.noreply.github.com>
2018-06-29 17:22:25 -0700

    Started using raster data in experiment.R +Adding columns with height, human influence, woody biomass data at each observation

--- experiment.R
+++ experiment.R
@@ -2,19 +2,26 @@
 #Occupancy analysis experimentation
 
 #Setup ----
+require("raster")
 require("lubridate")
 require("unmarked")
-occ.dat <- read.csv("~/Documents/MooringData-June2018-Clean.csv")
+occ.dat <- read.csv("~/Documents/MooringData-June2018-Clean.csv", stringsAsFactors = FALSE)
 
 occ.dat["YearMonth"] <- sapply(X = occ.dat$Date,
                                FUN = function(x) {paste(year(x), month(x))})
 
-occ.dat["Region"] <- apply(X = occ.dat,
-                           MARGIN = 1,
-                           FUN = function(x) {
-                              lon <- as.numeric(x["Longitude"])
-                              lat <- as.numeric(x["Latitude"])
-                              return(paste0(round(lon, digits = 2), ";", round(lat, digits = 2)))
-                            })
+height <- raster("GIS/Elevation_CostaRica.tif")
+hii <- raster("GIS/HII_NorthAmerica.tif")
+woodbiomass <- raster("GIS/WHRC_AbovegroundWodyBiomass.tif")
 
-print(unique(occ.dat$Region))
+coords <- data.frame("long" = occ.dat$Longitude, "lat" = occ.dat$Latitude)
+occ.dat["Height"] <- extract(x = height, y = coords)
+occ.dat["HumanInfluence"] <- extract(x = hii, y = coords)
+occ.dat["ForestBiomass"] <-- extract(x = woodbiomass, y = coords)
+
+cat("Height\n")
+print(summary(occ.dat$Height))
+cat("\nHuman\n")
+print(summary(occ.dat$HumanInfluence))
+cat("\nForest\n")
+print(summary(occ.dat$ForestBiomass))
